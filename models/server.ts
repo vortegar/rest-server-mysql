d2e00f08b9e4a66b5b71a8fae1b92d9355467534
Victorio Ortega <vortegar28@gmail.com>
2023-05-06 22:43:49 -0400

    primer commit

---
+++ models/server.ts
@@ -0,0 +1,62 @@
+import express, { Application } from 'express';
+import userRoutes from '../routes/usuario';
+import cors from 'cors'
+import db from '../db/connection';
+
+class Server {
+
+    private app: Application;
+    private port: string; 
+    private apiPaths = {
+        usuarios : '/api/usuarios'
+    }
+
+    constructor() {
+        this.app  = express();
+        this.port = process.env.PORT || '8000';
+
+        // Conexion con base de datos
+        this.dbConnection();
+        // Middlewares
+        this.middlewares();
+        // Definir mis rutas
+        this.routes();
+
+        // Ruta publica
+        this.app.use( express.static('public') );
+    }
+
+
+    async dbConnection(){
+
+        try {
+            
+            await db.authenticate();
+            console.log('Database online');
+            
+        } catch (error:any) {
+            throw new Error( error )
+        }
+    }
+
+    middlewares(){
+
+        // Cors
+        this.app.use( cors() );
+
+        // Lectura de body
+        this.app.use( express.json() );
+
+    }
+
+    routes(){
+        this.app.use( this.apiPaths.usuarios, userRoutes );
+    }
+    listen(){
+        this.app.listen( this.port, () => {
+            console.log(`Servidor Corriendo en puerto = ${ this.port }`)
+        })
+    }
+}
+
+export default Server;
